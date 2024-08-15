44ca346b2dfe7bb4a3fc31649d365af50aad941c
Davintus <kenechukwunzute@gmail.com>
2024-08-15 18:21:51 +0100

    .

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,16 +1,16 @@
+variable "credentials_file" {  
+  description = "Path to the service account credentials file"  
+  type        = string  
+  default     = "./credential.json"  # Ensure this line ends with a quote  
+}  
+
 variable "project_id" {  
   description = "The GCP project ID"  
   type        = string  
 }  
 
-variable "credentials_file" {  
-  description = "Path to the service account key file"  
-  type        = string
-  default     = "./credential.json" 
-}  
-
 variable "api_image_name" {  
   description = "The Docker image name for the API"  
   type        = string  
-  default     = "gcr.io/${var.project_id}/gcp-time-api"  
+  default     = "gcr.io/${var.project_id}/gcp-time-api"  # Correct interpolation  
 }
